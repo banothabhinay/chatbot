93af0a9b03edfeb52ef2ebb842d9f7f423e86c01
Abhinay <93334126+banothabhinay@users.noreply.github.com>
2023-01-28 19:21:14 +0530

    Add files via upload

---
+++ s2.js
@@ -0,0 +1,22 @@
+function getBotResponse(input){
+    if(input=="hi"){
+        return "hi bruh";
+
+    }
+    else if(input == "what are you doing"){
+        return "smg better than this";
+    }
+    else if(input == "rock"){
+        return "paper";
+    }
+    else if(input == "paper"){
+        return "scissors";
+    }
+    else if(input == "scissors"){
+        return "rock";
+    }
+    else{
+        return "idk wt u r saying im still learning";
+    }
+
+}
